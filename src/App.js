5f8c944fa18277da251dfafaac1b94c49b8bc301
koleganov <taggz1030@gmail.com>
2023-01-09 17:42:42 +0500

    'props'

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import Card from "./components/ Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/cards/card-1.png' },
+  { name: 'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl: '/img/cards/card-2.jpg' },
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/cards/card-3.jpg' },
+  { name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/cards/card-4.jpg' },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
       <Drawer />
-      <Header /> 
+      <Header />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
           <h1>Все кроссовки</h1>
@@ -18,55 +25,11 @@ function App() {
           </div>
         </div>
         <div className="sneakers d-flex">
-          <Card / >
-          <div className="card">
-            <img width={133} height={112} src="img/cards/card-2.jpg" alt="" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <span className="price">12 999 руб.</span>
-              </div>
-              <button className="button">
-                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <img width={133} height={112} src="img/cards/card-3.jpg" alt="" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <span className="price">12 999 руб.</span>
-              </div>
-              <button className="button">
-                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <img width={133} height={112} src="img/cards/card-4.jpg" alt="" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <span className="price">12 999 руб.</span>
-              </div>
-              <button className="button">
-                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10.6653 5.13122H7.20214V1.66821C7.20214 0.332846 5.13114 0.332846 5.13114 1.66821V5.13122H1.668C0.332935 5.13122 0.332935 7.20215 1.668 7.20215H5.13114V10.6652C5.13114 12.0005 7.20214 12.0005 7.20214 10.6652V7.20215H10.6653C12.0005 7.20215 12.0005 5.13122 10.6653 5.13122Z" fill="#D3D3D3" />
-                </svg>
-              </button>
-            </div>
-          </div>
-
+          {
+            arr.map(obj => (
+              <Card title={obj.name} price={obj.price} imageUrl={obj.imageUrl} />
+            ))
+          }
         </div>
       </div>
     </div>
